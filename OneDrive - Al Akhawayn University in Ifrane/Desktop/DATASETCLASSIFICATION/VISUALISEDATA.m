9d0e28b5be7abc7042a134a831318825ba943e3c
Adnane Ahroum <adnaneahroum69@gmail.com>
2025-04-24 16:31:44 +0100

    DATA add

---
+++ OneDrive - Al Akhawayn University in Ifrane/Desktop/DATASETCLASSIFICATION/VISUALISEDATA.m
@@ -0,0 +1,10 @@
+matFiles = dir('C:\Users\adnane\OneDrive - Al Akhawayn University in Ifrane\Desktop\DATASETCLASSIFICATION/*.mat');
+for k = 1:length(matFiles)
+    filePath = fullfile(matFiles(k).folder, matFiles(k).name);
+    data = load(filePath);
+    imageData = data.cjdata.image;
+    label = data.cjdata.label;
+    imshow(imageData, []);
+    title(['File: ', matFiles(k).name, ' | Tumor Class: ', num2str(label)]);
+    pause(1);  % Pause for 1 second between images
+end
